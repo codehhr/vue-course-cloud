0c2b4b2c3bf8990ead75ff29ae568e103b90f3fd
codehhr <1871973389@qq.com>
2021-07-27 20:12:52 +0800

    add ExclusiveCourse

---
+++ src/components/ExclusiveCourse.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><h1>啥也没有</h1></div>
+</template>
+
+<script>
+export default {
+  name: "ExclusiveCourse",
+};
+</script>
+
+<style scoped lang="less">
+h1 {
+  margin: 100px auto;
+}
+</style>
