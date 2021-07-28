3a6617d4eb354901fe596d62d84ff676391c9d5e
codehhr <1871973389@qq.com>
2021-07-28 20:58:13 +0800

    add breadcrumb-nav

--- src/components/CourseDetail/CourseDetail.vue
+++ src/components/CourseDetail/CourseDetail.vue
@@ -9,6 +9,19 @@
         </template>
       </PageHeader>
     </div>
+
+    <!-- 面包屑导航 start -->
+    <a-breadcrumb class="breadcrumb-nav">
+      <a-breadcrumb-item href="">
+        <a-icon type="home" />
+        <span>首页</span>
+      </a-breadcrumb-item>
+      <a-breadcrumb-item href="">
+        <span>视频详情</span>
+      </a-breadcrumb-item>
+    </a-breadcrumb>
+    <!-- 面包屑导航 end -->
+
     <PageFooter></PageFooter>
   </div>
 </template>
@@ -43,6 +56,8 @@ export default {
 }
 </style>
 <style lang="less">
+@main-width: 1200px;
+
 .page {
   .detailpage-header-outer {
     a.index {
@@ -52,5 +67,12 @@ export default {
       }
     }
   }
+  // 面包屑导航 start
+  .breadcrumb-nav {
+    margin: 10px auto;
+    width: @main-width;
+    text-align: left;
+  }
+  // 面包屑导航 end
 }
 </style>
