641078cb0779cf933b9fbbb08b2be76983f5fc64
codehhr <1871973389@qq.com>
2021-07-28 22:18:54 +0800

    set font-family

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,9 @@
   margin: 0;
   padding: 0;
   box-sizing: border-box !important;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, PingFang SC,
+    Hiragino Sans GB, Microsoft YaHei, Helvetica Neue, Helvetica, Arial,
+    sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
 }
 
 a {
