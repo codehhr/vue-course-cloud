0794f144edc3273633a5a858609e7167358b2f16
codehhr <1871973389@qq.com>
2021-07-30 20:10:39 +0800

    _

--- src/router/index.js
+++ src/router/index.js
@@ -3,30 +3,37 @@ import VueRouter from "vue-router";
 import Index from "../views/Index";
 import ExclusiveCourse from "../components/ExclusiveCourse";
 import CourseDetail from "../views/CourseDetail";
-import PersonalCenter from "../views/PersonalCenter";
+import store from "../store/index";
 
 Vue.use(VueRouter);
 
 const routes = [
+  // 首页
   {
     path: "/",
     name: "Index",
     component: Index,
   },
+  // 专属课程
   {
     path: "/exclusive-course",
     name: "ExclusiveCourse",
     component: ExclusiveCourse,
   },
+  // 首页课程列表
   {
     path: "/course-detail/:id",
     name: "CourseDetail",
     component: CourseDetail,
   },
+  // 个人中心
   {
     path: "/personal-center",
     name: "PersonalCenter",
-    component: PersonalCenter,
+    component: () => import("../views/PersonalCenter"),
+    meta: {
+      requiredLogin: true,
+    },
   },
 ];
 
@@ -34,6 +41,20 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  // 判断是否是需要登录的页面
+  // 再判断是否是登陆状态
+  if (to.meta.requiredLogin) {
+    if (!store.state.alreadyLogin) {
+      next("/");
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
