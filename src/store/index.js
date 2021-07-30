e8bbfea5f76a7c5629b0956a67f4c5106446407f
codehhr <1871973389@qq.com>
2021-07-30 16:54:52 +0800

    user Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,41 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import { getUserInfo } from "../api/api";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
+  // 全局状态
   state: {
+    // 登录状态
+    alreadyLogin: false,
+    // 用户信息
+    userInfo: null,
   },
+  // 同步修改全局状态
   mutations: {
+    setUserInfoAndLoginStatus(state, payload) {
+      state.alreadyLogin = payload.alreadyLogin;
+      state.userInfo = payload.userInfo;
+    },
   },
   actions: {
+    // 检查登录状态
+    checkAlreadyLogin(context) {
+      getUserInfo().then((res) => {
+        if (res.code === 0) {
+          context.commit("setUserInfoAndLoginStatus", {
+            alreadyLogin: true,
+            userInfo: res.userInfo,
+          });
+        } else {
+          context.commit("setUserInfoAndLoginStatus", {
+            alreadyLogin: false,
+            userInfo: null,
+          });
+        }
+      });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
