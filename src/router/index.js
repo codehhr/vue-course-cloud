d11306750589b5c7b2570d8d07d6696bb3ae62e7
codehhr <1871973389@qq.com>
2021-07-30 22:02:22 +0800

    create CourseVideo component

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Index from "../views/Index";
 import ExclusiveCourse from "../components/ExclusiveCourse";
 import CourseDetail from "../views/CourseDetail";
 import store from "../store/index";
+import CourseVideo from "../components/CourseDetail/CourseVedio";
 
 Vue.use(VueRouter);
 
@@ -26,6 +27,12 @@ const routes = [
     name: "CourseDetail",
     component: CourseDetail,
   },
+  // 课程视频
+  {
+    path: "/course-detail/course-video",
+    name: "course-video",
+    component: CourseVideo,
+  },
   // 个人中心
   {
     path: "/personal-center",
@@ -46,7 +53,8 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   // 判断是否是需要登录的页面
   // 再判断是否是登陆状态
-  if (to.meta.requiredLogin) {
+  if (to.path == "/personal-center") {
+    console.log(store.state.alreadyLogin);
     if (!store.state.alreadyLogin) {
       next("/");
     } else {
